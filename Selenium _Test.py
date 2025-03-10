c5915e7b9da2d66b8f8e4e74c89f7a08e6e5505f
unknown <you@example.com>
2025-03-10 21:31:54 +0100

    update

--- Selenium _Test.py
+++ Selenium _Test.py
@@ -2,20 +2,19 @@ import time
 
 import py
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
 driver.get("https://automationplayground.com/crm/login.html")
 time.sleep(5)
 
-driver.find_element(By.ID, "email-id").send_keys("confixhub@yopmail.com")
-time.sleep(5)
-driver.find_element(By.ID, "password").send_keys("Debbie19")
-time.sleep(5)
-driver.find_element(By.ID, "remember").click()
-time.sleep(5)
-driver.find_element(By.ID, "submit-name").click()
-time.sleep(5)
+#driver.find_element(By.ID, "email-id").send_keys("confixhub@yopmail.com")
+#time.sleep(5)
+#driver.find_element(By.ID, "password").send_keys("Debbie19")
+#time.sleep(5)
+#driver.find_element(By.ID, "remember").click()
+#time.sleep(5)
+#driver.find_element(By.ID, "submit-name").click()
+#time.sleep(5)
 
 
